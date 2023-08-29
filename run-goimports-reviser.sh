157583109db2a7af7f0e228e54cdda82bc80c0a7
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2023-08-28 18:15:00 -0600

    chore: add forward arguments to goimports-reviser hook (#8)

--- run-goimports-reviser.sh
+++ run-goimports-reviser.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2124
 
 set -e -o pipefail
 
+command="goimports-reviser -format"
+
+if [ $# -gt 0 ]; then
+  command="$command $@"
+fi
+
+command="$command ./..."
+
+echo "Running: $command"
+
 exec 5>&1
-output="$(goimports-reviser -format ./... | tee /dev/fd/5)"
+output="$($command | tee /dev/fd/5)"
 [[ -z "$output" ]]
