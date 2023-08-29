797404b81c826202be571186204cf9daa7dfe6cc
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2023-08-29 12:00:08 -0600

    feat: add hook to execute revive lint (#9)

---
+++ run-revive.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2124
+
+set -e -o pipefail
+
+command="revive"
+
+if [ $# -gt 0 ]; then
+  command="$command $@"
+fi
+
+command="$command ./..."
+
+echo "Running: $command"
+
+exec 5>&1
+output="$($command | tee /dev/fd/5)"
+[[ -z "$output" ]]
