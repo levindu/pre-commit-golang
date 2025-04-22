989b44cb93ab73ae49bebfd16fc7aed90a297757
Levin Du <zslevin@gmail.com>
2025-04-22 09:40:51 +0800

    cleanup go-mod-tidy and run command in subshell

--- run-go-mod-tidy.sh
+++ run-go-mod-tidy.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-function get_base_dir() {
-  local file="$1"
-  local dir=$(dirname "$file")
-  echo "$dir"
-}
-
 # Find and process go.mod files
 go_mod_files=$(find . -type f -name "go.mod")
 if [ -n "$go_mod_files" ]; then
   for file in $go_mod_files; do
-    cd "$(get_base_dir "$file")" && go mod tidy -v
-
+    (
+        cd "$(dirname "$file")" &&
+         go mod tidy -v
+    )
     if [ $? -ne 0 ]; then
       echo "go mod tidy failed for $file"
       exit 2
@@ -19,5 +15,4 @@ if [ -n "$go_mod_files" ]; then
   done
 fi
 
-git add --all
 exit 0
